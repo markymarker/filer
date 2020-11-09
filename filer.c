e164b98b17f5a0df7eb40d52ca6caed96d439ca3
markymarker <7markf@gmail.com>
2020-11-09 16:45:32 -0700

    Initial commit

---
+++ filer.c
@@ -0,0 +1,301 @@
+// 2020-11-04
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEBUG 1
+
+#if DEBUG
+#define DEBUGPRINT(t) printf("DEBUG: %s\n", t);
+#else
+#define DEBUGPRINT(t) 
+#endif
+
+#define GENERIC_BUF_SIZE 256
+#define FB_MAX_BUF_SIZE 20480 // 20 KiB
+#define LABEL_MAX_SIZE 64
+
+#define FB_INITIALIZED 0x01
+#define FB_LOADED_LABELS 0x04
+
+const static char filespath[] = "./dfiles/";
+
+
+typedef struct {
+  long int fpos; // Position of label in file
+  char   * text; // Pointer into fb.label_texts for text of label
+} label;
+
+typedef struct {
+  const char * fname;       // File name
+  FILE       * fhandle;     // Handle for open file
+  char       * label_texts; // Block of memory to hold all labels
+  label      * labels;      // Block of memory to hold all label structs
+  long int     fsize;       // File size
+  int          label_count; // Number of labels in file
+  char         operations;  // Indicator for actions performed
+//char         file_in_buf; // Boolean indicating if entire file is in buf
+  char         no_use_buf;  // Boolean to prevent loading buf, if desired
+  char       * buf;         // Buffer for file contents if within set limit
+} fileblock;
+
+
+long int get_file_size(FILE *);
+int dump_file_contents(FILE *);
+
+int init_fileblock(fileblock *);
+int close_fileblock(fileblock *);
+int load_fileblock_file_maybe(fileblock *);
+int load_fileblock_labels(fileblock *);
+
+
+/* The main event.
+ */
+int main(int argl, char ** argv){
+
+  const char * fname;
+
+  if(argl > 1){
+    fname = argv[1];
+  } else {
+    fname = filespath;
+  }
+
+  fileblock fblock = {
+    .fname = fname,
+    //.no_use_buf = 1,
+  };
+  fileblock * fb = &fblock;
+  init_fileblock(fb);
+
+  printf("Opened fileblock for: %s\n", fb->fname);
+  printf("Size: %ld\n", fb->fsize);
+
+  if(fb->buf != NULL){
+    printf("Contents of buffer:\n");
+    fwrite(fb->buf, sizeof(char), fb->fsize, stdout);
+    printf("\n");
+  } else {
+    printf("Buffer not used\n");
+  }
+
+  close_fileblock(fb);
+  printf("Closed fileblock for: %s\n", fb->fname);
+
+  return 0;
+}
+
+
+/* Initializes a fileblock by inspecting the file to fill its fields.
+ * The fileblock pass to this function should have a filename set in the fname field.
+ */
+int init_fileblock(fileblock * fb){
+  if(fb == NULL) return 1;
+  if(fb->fname == NULL) return 2;
+
+  fb->operations &= ~(FB_INITIALIZED);
+
+  // Clean up potential old data from fileblock
+  close_fileblock(fb);
+
+  if(fb->operations != 0)
+    fprintf(stderr, "Warning: fb operations not clear (%d)\n", (int)fb->operations);
+
+  // The following are already set appropriately by close_fileblock
+  // fb->label_texts
+  // fb->labels
+  // fb->label_count
+  // fb->operations
+  // fb->file_in_buf
+  // fb->buf
+
+  // Load new data into fileblock
+
+  fb->fhandle = fopen(fb->fname, "r");
+  if(fb->fhandle == NULL) return 3;
+
+  fb->fsize = get_file_size(fb->fhandle);
+  if(!fb->fsize){
+    fprintf(stderr, "Error in getting file size\n");
+    return 3;
+  }
+
+  load_fileblock_file_maybe(fb);
+
+  load_fileblock_labels(fb);
+
+  // TODO
+
+  // Set to initialized only
+  fb->operations = FB_INITIALIZED;
+
+  return 0;
+}
+
+
+/* Clean up a fileblock and free all memory that was allocated.
+ */
+int close_fileblock(fileblock * fb){
+  if(fb == NULL) return 1;
+
+  if(fb->fhandle != NULL){
+    DEBUGPRINT("Cleaning up opened file");
+    if(fclose(fb->fhandle) != 0){
+      fprintf(stderr, "Error closing already opened file\n");
+      return  2;
+    }
+    fb->fhandle = NULL;
+  }
+
+  if(fb->labels != NULL) free(fb->labels);
+  else if(fb->label_count){
+    DEBUGPRINT("No label pointer to clean up");
+  }
+  fb->labels = NULL;
+
+  if(fb->label_texts != NULL) free(fb->label_texts);
+  else if(fb->label_count){
+    DEBUGPRINT("No label text pointer to clean up");
+  }
+  fb->label_texts = NULL;
+
+  // Labels cleared, so clear action flag
+  fb->operations &= ~(FB_LOADED_LABELS);
+
+  // Not strictly a part of closing the fileblock, but since we're clearing the
+  // buffers, we will also set these to keep state consistent
+  fb->label_count = 0;
+  //fb->file_in_buf = 0;
+
+  //memset(fb->buf, '\0', FB_MAX_BUF_SIZE);
+
+  return 0;
+}
+
+
+/* If the size of the file is less than the defined threshold, create a buffer
+ * and load the file contents into it. If the size is larger than the threshold,
+ * do nothing.
+ */
+int load_fileblock_file_maybe(fileblock * fb){
+  // No use buf flag set -- not an error condition
+  if(fb->no_use_buf) return 0;
+  // File larger than threshold -- not an error condition
+  if(fb->fsize > FB_MAX_BUF_SIZE) return 0;
+
+  size_t buf_size = fb->fsize * sizeof(char);
+
+  // Create a buffer to match the file size
+  fb->buf = (char *)malloc(buf_size);
+  if(fb->buf == NULL){
+    fprintf(stderr, "Error allocating whole-file buffer\n");
+    return 1;
+  }
+
+  rewind(fb->fhandle);
+
+  char reset_buf = 0;
+  size_t read_bytes = fread(fb->buf, sizeof(char), fb->fsize, fb->fhandle);
+
+  if(read_bytes != buf_size){
+    // Print general info
+    fprintf(
+      stderr,
+      "Mismatch between read size (%llu) and file size (%lu): ",
+      (long long)read_bytes,
+      fb->fsize
+    );
+
+    // Determine and print more specific info
+    if(feof(fb->fhandle)){
+      fprintf(stderr, "End of file reached while reading\n");
+    } else if(ferror(fb->fhandle)){
+      fprintf(stderr, "Read error occurred\n");
+      perror("Error message");
+      reset_buf = 1;
+    } else {
+      fprintf(stderr, "Unknown reason for mismatch\n");
+      reset_buf = 1;
+    }
+  }
+
+  if(reset_buf){
+    free(fb->buf);
+    fb->buf = NULL;
+    return 1;
+  }
+
+  return 0;
+}
+
+
+/* Reads the labels from the configured file in the provided initialized
+ * fileblock and stores them properly into said fileblock.
+ *
+ * This function uses the buf in the fileblock.
+ */
+int load_fileblock_labels(fileblock * fb){
+  if(fb == NULL) return 1;
+  if(!(fb->operations | FB_INITIALIZED)) return 2;
+  if(fb->operations & FB_LOADED_LABELS) return 3;
+
+  FILE * f = fb->fhandle;
+  char * buf = fb->buf;
+
+  rewind(f);
+
+  // First pass: Figure out how many labels are present
+
+  //
+}
+
+
+/* Print out the size of the given file (or so).
+ * Returns a negative value on error.
+ * May be improved by using e.g. fstat
+ */
+long int get_file_size(FILE * f){
+  if(f == NULL) return -1L;
+
+  long int ppos = ftell(f);
+
+  if(fseek(f, 0, SEEK_END) != 0){
+    perror("Error occurred while seeking");
+    return -1L;
+  }
+
+  long int size = ftell(f);
+
+  if(fseek(f, ppos, SEEK_SET) != 0){
+    perror("Error occurred while seeking to reset");
+    // Okay to continue, but position in file has likely changed
+  }
+
+  return size;
+}
+
+
+/* Dump out the contents of the given file.
+ */
+int dump_file_contents(FILE * f){
+  if(f == NULL) return 1;
+
+  rewind(f);
+
+  printf(":::Contents Start:::\n");
+  char buf[GENERIC_BUF_SIZE] = {0};
+  do {
+    int to_read = GENERIC_BUF_SIZE - 1;
+    int read = fread((void *)buf, sizeof(char), to_read, f);
+    buf[read] = '\0';
+    fprintf(stdout, "%s", buf);
+
+    if(read < to_read){
+      if(!feof(f)) perror("Error occurred in reading file");
+      break;
+    }
+  } while(1);
+  printf(":::Contents End:::\n");
+}
+
---
+++ state_machine.c
@@ -0,0 +1,230 @@
+/* 2020-11-09
+ *
+ * This is a state machine to parse a block of text and search for labels.
+ * Labels consist of a line of text which has a preceeding line that begins with
+ * 5 equals signs (=). The preceeding line is not a part of the label.
+ */
+
+#include <stdio.h>
+
+typedef int (* generic_func)(char);
+typedef generic_func (* sm_func)(char);
+
+
+int run_iteration(sm_func *, char, char *);
+sm_func entry(char);
+sm_func ignore_line(char);
+sm_func delim_matched_finish_line(char);
+sm_func label_line(char);
+sm_func
+  equals1(char),
+  equals2(char),
+  equals3(char),
+  equals4(char)
+;
+
+
+int main(int argl, char ** argv){
+  char str[] = "abcdefg\n======abc==\nlabel a:\nhijklmnop\n=====\nANOTHER_LABEL:\nqq";
+  printf("Running state machine on the following:\n%s\n\n", str);
+
+  char lbuf1[21];
+  char lbuf2[21];
+  char lbuf3[21];
+  char * lbufs[] = {lbuf1, lbuf2, lbuf3};
+  //lbuf1[0] = lbuf2[0] = lbuf3[0] = '\0';
+  char * c = str;
+  sm_func smf = NULL;
+
+  for(int lbuf = 0; lbuf < 3; ++lbuf){
+    if(*c == '\0') break;
+
+    int rval;
+    int llen = 0;
+
+    while(*c != '\0'){
+      char * ls = (llen < 20) ? lbufs[lbuf] + llen : NULL;
+      rval = run_iteration(&smf, *c, ls);
+      ++c;
+      if(rval <= 0) break;
+      if(rval == 2) ++llen;
+    }
+
+    lbufs[lbuf][llen] = '\0';
+
+    if(rval < 0){
+      printf("Error occured in state machine: %d\n", rval);
+      break;
+    }
+  }
+
+  /*
+  for(int lbuf = 0; lbuf < 3; ++lbuf){
+    if(*c == '\0') break;
+
+    sm_func state = (sm_func)entry;
+    int llen = 0;
+
+    while(state != NULL && *c != '\0'){
+      if(state == (sm_func)label_line && llen < 20)
+        if(*c != ':' && *c != '\n')
+          lbufs[lbuf][llen++] = *c;
+      state = (sm_func)state(*c);
+      ++c;
+    }
+
+    lbufs[lbuf][llen] = '\0';
+  }
+  */
+
+  printf("Finished\n");
+  printf("Found labels:\n  1: %s\n  2: %s\n  3: %s\n", lbuf1, lbuf2, lbuf3);
+}
+
+
+/* Run a single iteration of the state machine.
+ *
+ * This will handle the reassigning and checking of the states, which is held
+ * by the caller and provided with the parameter `f`, which should initially be
+ * set to NULL.
+ *.
+ * If a label state is detected, the current character will be stored into the
+ * provided `lstore` parameter, if not null.
+ *
+ * Return values:
+ *  <0: Error
+ *   0: Finished
+ *  >0: Still running:
+ *      1: Normal continuing
+ *      2: Label character stored
+ */
+int run_iteration(sm_func * f, char c, char * lstore){
+  if(*f == NULL) *f = (sm_func)entry;
+  int rval = 1;
+
+  *f = (sm_func)(*f)(c);
+  if(*f == (sm_func)label_line && lstore != NULL){
+    if( c == ' ' || c == '_' || c == '-'
+    || (c >= 'a' && c <= 'z')
+    || (c >= 'A' && c <= 'Z')
+    ){
+      *lstore = c;
+      rval = 2;
+    }
+  } else if(*f == NULL){
+    rval = 0;
+  }
+
+  return rval;
+}
+
+
+/* Entry state
+ * The beginning of a line
+ * Looking for an equals sign, failure ignores the line
+ */
+sm_func entry(char c){
+  printf("In entry state: %c\n", c);
+
+  switch(c){
+  case '=': return (sm_func)equals1;
+  default: return ignore_line(c);
+  }
+}
+
+
+/* Ignore line state
+ * Looking for newline
+ */
+sm_func ignore_line(char c){
+  printf("In ignore line state: %c\n", c);
+
+  switch(c){
+  case '\n': return (sm_func)entry;
+  default: return (sm_func)ignore_line;
+  }
+}
+
+
+/* Delimited matched state
+ * Looking for newline
+ */
+sm_func delim_matched_finish_line(char c){
+  printf("In delim matched state: %c\n", c);
+
+  switch(c){
+  case '\n': return (sm_func)label_line;
+  default: return (sm_func)delim_matched_finish_line;
+  }
+}
+
+
+/* Label line state
+ * Looking for newline
+ */
+sm_func label_line(char c){
+  printf("In label line state: %c\n", c);
+
+  switch(c){
+  //case '\n': return (sm_func)entry;
+  case '\n': return NULL;
+  default: return (sm_func)label_line;
+  }
+}
+
+
+/* Equals 1 state
+ * One equals found
+ * Looking for an equals sign, failure ignores the line
+ */
+sm_func equals1(char c){
+  printf("In equals 1: %c\n", c);
+
+  switch(c){
+  case '=': return (sm_func)equals2;
+  default: return ignore_line(c);
+  }
+}
+
+
+/* Equals 2 state
+ * Two equals found
+ * Looking for an equals sign, failure ignores the line
+ */
+sm_func equals2(char c){
+  printf("In equals 2: %c\n", c);
+
+  switch(c){
+  case '=': return (sm_func)equals3;
+  default: return ignore_line(c);
+  }
+}
+
+
+/* Equals 3 state
+ * Three equals found
+ * Looking for an equals sign, failure ignores the line
+ */
+sm_func equals3(char c){
+  printf("In equals 3: %c\n", c);
+
+  switch(c){
+  case '=': return (sm_func)equals4;
+  default: return ignore_line(c);
+  }
+}
+
+
+/* Equals 4 state
+ * Four equals found
+ * Looking for an equals sign, failure ignores the line
+ */
+sm_func equals4(char c){
+  printf("In equals 4: %c\n", c);
+
+  switch(c){
+  case '=': return (sm_func)delim_matched_finish_line;
+  default: return ignore_line(c);
+  }
+}
+
