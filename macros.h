cdf84595f68853e6900dc995d91db6650c4259ac
markymarker <7markf@gmail.com>
2020-11-10 17:16:02 -0700

    Adding new debug print type and adjusting level

--- macros.h
+++ macros.h
@@ -1,12 +1,14 @@
 
-#define DEBUG 2
+#define DEBUG 1
 
 #if DEBUG > 0
   #define DEBUGPRINT(t) printf("DEBUG: %s\n", t);
   #define DEBUGPRINTC(t, c) printf("DEBUG: %s: %c\n", t, c);
+  #define DEBUGPRINTD(t, d) printf("DEBUG: %s: %d\n", t, d);
 #else
   #define DEBUGPRINT(t) ;
   #define DEBUGPRINTC(t, c) ;
+  #define DEBUGPRINTD(t, d) ;
 #endif
 
 #if DEBUG > 1
